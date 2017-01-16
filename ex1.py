986d8a06041e9f9d8d7257497078565f3642a1a9
xunyun <forforture@gmail.com>
2017-01-16 15:50:47 +0800

    python

---
+++ ex1.py
@@ -0,0 +1,6 @@
+print "Hello World!"
+print "Hello Again"
+print "I like typing this"
+print "This is fun"
+print 'Yay!printing.'
+print 'I "said" do not touch this'
---
+++ ex3.py
@@ -0,0 +1,29 @@
+# Day 1
+
+print "I will now count my chickens:"
+
+print "Hens", 25 + 30 / 6
+print "Roosters", 100 - 20 * 3 % 4
+
+print "Now I will count the eggs:"
+
+print 3 + 2 +1-5+4%2-1/4+6
+
+print "Is it ture that 3+2<5-7?"
+
+print 3+2<5-7
+
+print "what is 3+2?",3+2
+print "What is 5-7?",5-7
+
+print "Oh,that's why it's False."
+
+print "How about some more."
+
+print "Is it gteater?",5>2
+print "Is it greater or equal?",5>=-2
+print "Is it less or equal?",5<=-2
+
+print "7/5?",7/5
+print "7.0/6.0?",7.0/6.0
+
